2d44409373a2f477d8f8b608cc1a0f97e29cdf0f
ArmandoMtrz <am19133@ues.edu.sv>
2024-04-04 20:58:21 -0600

    Actualizaciones

--- bresenham.c
+++ bresenham.c
@@ -1,4 +1,4 @@
-//Autor: José Armando Alarcón
+// Autor: José Armando Alarcón
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
@@ -7,32 +7,32 @@ void bersenham(int x0, int y0, int xn, int yn) {
     int dx = abs(xn - x0);
     int dy = abs(yn - y0);
     int M = dy / dx;
-    int decisionFactor = 2 * dy - dx;
+    int factorDecision = 2 * dy - dx;
     int x = x0;
     int y = y0;
 
-    // Check if the origin is smaller than the destination
+    // Verificar si el origen es menor que el destino
     if (x0 > xn || y0 > yn) {
-        printf("Invalid input: Origin should be smaller than the destination.\n");
+        printf("Entrada inválida: El origen debe ser menor que el destino.\n");
         return;
     }
 
-    // Draw the line
-    printf("Line: (%d, %d) -> (%d, %d)\n", x0, y0, xn, yn);
-    printf("Points: (%d, %d)\n", x, y);
+    // Dibujar la línea
+    printf("Línea: (%d, %d) -> (%d, %d)\n", x0, y0, xn, yn);
+    printf("Puntos: (%d, %d)\n", x, y);
 
     glBegin(GL_POINTS);
     glVertex2i(x, y);
 
     while (x < xn) {
-        if (decisionFactor <= 0) {
-            decisionFactor += 2 * dy;
+        if (factorDecision <= 0) {
+            factorDecision += 2 * dy;
         } else {
-            decisionFactor += 2 * (dy - dx);
+            factorDecision += 2 * (dy - dx);
             y++;
         }
         x++;
-        printf("Points: (%d, %d)\n", x, y);
+        printf("Puntos: (%d, %d)\n", x, y);
         glVertex2i(x, y);
     }
     glEnd();
@@ -43,9 +43,9 @@ void display() {
     glColor3f(1.0, 1.0, 1.0);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluOrtho2D(0, 500, 0, 500);
+    gluOrtho2D(0, 700, 0, 700);
 
-    // Change the coordinates as needed
+    // Cambiar las coordenadas según sea necesario
     bersenham(2, 1, 8, 5);
 
     glFlush();
@@ -56,10 +56,11 @@ int main(int argc, char** argv) {
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
     glutInitWindowPosition(100, 100);
-    glutCreateWindow("Bresenham Line Drawing");
+    glutCreateWindow("Dibujo de línea de Bresenham");
     glutDisplayFunc(display);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glutMainLoop();
 
     return 0;
 }
+
